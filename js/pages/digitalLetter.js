10238e4af7a614310aa2577b352346ce170a39bb
SantiPelaez07 <santipelaez0@gmail.com>
2024-09-27 00:05:30 -0500

    Mejora y avance en la página principal

---
+++ js/pages/digitalLetter.js
@@ -0,0 +1,8 @@
+// Funcionalidades: 
+// Botón poder acceder a la página donde está la carta
+const btnDigitalLetter = document.querySelector(".digitalLetter");
+
+btnDigitalLetter.addEventListener("click", () => {
+    window.location.href = "pages/menu.html"
+  })
+

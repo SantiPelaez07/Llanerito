ca942ba0b1d1d671c301aefa4dcb5b4f96fbbe0f
SantiPelaez07 <santipelaez0@gmail.com>
2025-04-22 21:23:56 -0500

    Improvements

--- js/pages/digitalLetter.js
+++
@@ -1,8 +0,0 @@
-// Funcionalidades: 
-// Botón poder acceder a la página donde está la carta
-const btnDigitalLetter = document.querySelector(".digitalLetter");
-
-btnDigitalLetter.addEventListener("click", () => {
-    window.location.href = "pages/menu.html"
-  })
-
